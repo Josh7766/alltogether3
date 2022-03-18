3e8a58815198362e95b22d9916a104e9cd7a4159
Jakeod3 <101876741+Jakeod3@users.noreply.github.com>
2022-03-18 10:04:30 -0400

    Update pxt.json, main.blocks, tilemap.g.jres, tilemap.g.ts, images.g.jres, images.g.ts

---
+++ images.g.ts
@@ -0,0 +1,56 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "player 1":return img`
+. . . . f f f f . . . . 
+. . f f f f f f f f . . 
+. f f f f f f f f f f . 
+f f f f f f f f f f f f 
+2 2 2 2 2 2 2 2 2 2 2 2 
+1 1 1 1 1 1 1 1 1 1 1 1 
+2 2 2 2 2 2 2 2 2 2 2 2 
+f 4 4 f f 4 4 f f 4 4 f 
+f e 4 d d d d d d 4 e f 
+. f e d d b b d d e f . 
+. f f e 4 4 4 4 e f f . 
+e 4 f 2 2 f 2 f 2 f 4 e 
+4 d f 2 2 f 2 f 2 f d 4 
+4 4 f 2 2 2 2 2 2 f 4 4 
+. . . f f f f f f . . . 
+. . . f f . . f f . . . 
+`;
+            case "image2":
+            case "player2":return img`
+. . . . f f f f . . . . 
+. . f f f f f f f f . . 
+. f f f f f f f f f f . 
+f f f f f f f f f f f f 
+8 8 8 8 8 8 8 8 8 8 8 8 
+1 1 1 1 1 1 1 1 1 1 1 1 
+8 8 8 8 8 8 8 8 8 8 8 8 
+f 4 4 f f 4 4 f f 4 4 f 
+f e 4 d d d d d d 4 e f 
+. f e d d b b d d e f . 
+. f f e 4 4 4 4 e f f . 
+e 4 f 8 8 f 8 f 8 f 4 e 
+4 d f 8 8 f 8 f 8 f d 4 
+4 4 f 8 8 8 8 8 8 f 4 4 
+. . . f f f f f f . . . 
+. . . f f . . f f . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
